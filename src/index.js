e7566f271685ddae7676f2e667269bb23a33b71d
unknown <asikumar@WKWIN6972966.global.publicisgroupe.net>
2018-07-23 18:04:55 +0530

    third assignment

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// import '../css/style.css';
+// import 'bootstrap';
+// // import $ from 'jquery';
+// import $ from 'jquery';
+
+
+//  function boardfunction()
+// {
+//     var button= document.getElementById('bid');
+//     button.onclick =function myFuntion(){
+//     var idiv = document.getElementById('board');
+//     var firstDiv = document.createElement('div');
+//     firstDiv.className = 'boards';
+//     firstDiv.draggable=true;
+//     idiv.appendChild(firstDiv);
+//     var secondDiv = document.createElement('div');
+//     firstDiv.appendChild(secondDiv);
+//     secondDiv.className = 'boardTitle';
+//     var bname= document.getElementById("btitle");
+//     var printho=  document.getElementsByClassName("boardTitle") 
+//     secondDiv.innerHTML = secondDiv.innerHTML + bname.value
+//    // document.body.appendChild(firstDiv);   
+
+   
+//     }
+   
+// }
+// document.querySelector('#myModal button').addEventListener('click', boardfunction);
+
